1c8d64449ea364c1857987ba3681d6ce3c3c91a3
Huyen Bui Thanh Nguyen <nbthanhhuyen@gmail.com>
2015-12-29 22:55:58 -0500

    remove unwanted lines of code from server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var d3_dsv = require("d3-dsv");
 var fs = require('fs');
-// var d3_dsv = d3_dsv.dsv(",");
 var app = express();
 var port = process.env.PORT || 3000;
-
 var csv = d3_dsv.dsv(",");
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,7 +14,6 @@ app.listen(port, function() {
 	console.log('App is listening on port ' + port);
 });
 
-// console.log(fs.readFileSync(__dirname + '/data.csv').toString());
 var data = csv.parse(fs.readFileSync(__dirname + '/data.csv').toString(), function(d) {
   return {
     major: d.major,
@@ -27,17 +24,10 @@ var data = csv.parse(fs.readFileSync(__dirname + '/data.csv').toString(), functi
 
 app.get('/index', function(req, res) {
 	var arrObj = [];
-	// var html = "[";
 	data.forEach( function(object, i) {
-			// html += JSON.stringify(object) + ",";
-			arrObj.push(object);
+		arrObj.push(object);
 	})
 	var arrObjString = JSON.stringify(arrObj);
 	res.writeHead(200, {"Content-Type": "text/html"});
-	// res.end(html + "]");
 	res.end(arrObjString);
 });
-
-// console.log(data);
-
-
