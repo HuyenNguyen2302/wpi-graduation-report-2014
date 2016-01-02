27239f5dc086b5468c9eeba2cbdb5e5c56498ccf
Huyen Nguyen <nbthanhhuyen@gmail.com>
2016-01-01 19:27:31 -0500

    vertical grouped bar chart works now - reference: http://bl.ocks.org/mbostock/3887051

--- public/js/index.js
+++ public/js/index.js
@@ -2,53 +2,97 @@ var chart = document.getElementById("chart");
 
 d3.csv('data.csv', function(err, d) {
   console.log(d);
-  // TODO build stacked bar
-})
+  
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
-makeGetRequest('/index');
+  var x0 = d3.scale.ordinal()
+  .rangeRoundBands([0, width], .1);
 
-function makeGetRequest(url) {
-  var req = new XMLHttpRequest();
-  req.onload = function() {
-    var arrObj = JSON.parse(this.responseText);  
-    var undergradSalaryArr = [];
-    var masterSalaryArr = [];
-    arrObj.forEach(function (object, i) {
-      undergradSalaryArr.push(parseInt(object.undergrad) / 1000);
-      masterSalaryArr.push(parseInt(object.master) / 1000);
+  var x1 = d3.scale.ordinal();
 
-    var width = 420,
-    barHeight = 20;
+  var y = d3.scale.linear()
+  .range([height, 0]);
 
-    var x = d3.scale.linear()
-    .domain([0, d3.max(undergradSalaryArr)])
-    .range([0, width]);
+  var color = d3.scale.ordinal()
+  .range(["#98abc5", "#8a89a6"]); // , "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-    var chart = d3.select("#chart")
-    .attr("width", width)
-    .attr("height", barHeight * undergradSalaryArr.length);
+  var xAxis = d3.svg.axis()
+  .scale(x0)
+  .orient("bottom");
 
-    var bar = chart.selectAll("g")
-    .data(undergradSalaryArr)
+  var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left")
+  .tickFormat(d3.format(".2s"));
+
+  var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.csv("data.csv", function(error, data) {
+    if (error) throw error;
+
+    var salary_groups = d3.keys(data[0]).filter(function(key) { return key !== "major"; });
+
+    data.forEach(function(d) {
+      d.salaries = salary_groups.map(function(name) { return {name: name, value: +d[name]}; });
+    });
+
+    x0.domain(data.map(function(d) { return d.major; }));
+    x1.domain(salary_groups).rangeRoundBands([0, x0.rangeBand()]);
+    y.domain([0, d3.max(data, function(d) { return d3.max(d.salaries, function(d) { return d.value; }); })]);
+
+    svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+    svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Salary");
+
+    var major = svg.selectAll(".major")
+    .data(data)
     .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    .attr("class", "major")
+    .attr("transform", function(d) { return "translate(" + x0(d.major) + ",0)"; });
 
-    bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
+    major.selectAll("rect")
+    .data(function(d) { return d.salaries; })
+    .enter().append("rect")
+    .attr("width", x1.rangeBand())
+    .attr("x", function(d) { return x1(d.name); })
+    .attr("y", function(d) { return y(d.value); })
+    .attr("height", function(d) { return height - y(d.value); })
+    .style("fill", function(d) { return color(d.name); });
 
-    bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
+    var major = svg.selectAll(".major")
+    .data(salary_groups.slice().reverse())
+    .enter().append("g")
+    .attr("class", "major")
+    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    major.append("rect")
+    .attr("x", width - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", color);
+
+    major.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
     .attr("dy", ".35em")
+    .style("text-anchor", "end")
     .text(function(d) { return d; });
-    })
-  }
-  req.open('GET', url);
-  req.send();
-}
-
-function type(d) {
-  d.value = +d.value; // coerce to number
-  return d;
-}
+  });
+});
